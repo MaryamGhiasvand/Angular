155acd637d09e42b4454a0c3bd690c2bb14d4b33
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-06-06 19:28:44 -0500

    Add event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  //for element button we have (event) with small case
+
+  //u can send the entire event as parameter ==> access in template with $Event
+  template : '<button (click)="clickme($event)">Click me</button>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'AngularProject';
+  clickme(e){
+    console.log('btn clicked',e);
+    alert('clicked');
+  }
 }
