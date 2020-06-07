0e4d10e94f09feeb0cb63f2cc0f21e1331f4abc6
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-06-06 20:34:29 -0500

    Add 2way binding and attr binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,26 @@ import { Component } from '@angular/core';
   //for element button we have (event) with small case
 
   //u can send the entire event as parameter ==> access in template with $Event
-  template : '<button (click)="clickme($event)">Click me</button>',
+  template : '<p> <input type="text" #myInput><br/><button (click)="clickme($event,myInput)">Click me</button>'+
+  '<p>test attr + event Binding </p>'+
+  //access variable in template directly , in component with this
+  '<p> <input type="text" [value]="attrBind" (input)="attrBind=$event.target.value"></p> {{attrBind}}'+
+  //[value] = propertyBinding => for bind attrBind to input
+  //(input) is event to call related method and change the value in {{}}
+  '<p>test TwoWay Binding </p>'+
+  '<p> <input [(ngModel)]="twoWayVar" (keyup.enter)="twoWayBinding()"> </p>',
+
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'AngularProject';
-  clickme(e){
+  attrBind="DefaultAttrVal";
+  twoWayVar="Default2WayVal"
+  twoWayBinding(){
+    alert('twoWayVar : '+ this.twoWayVar);
+  }
+  clickme(e,myInput :HTMLInputElement){
     console.log('btn clicked',e);
-    alert('clicked');
+    alert('inputText : '+ myInput.value);
   }
 }
