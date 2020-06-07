bea0f6a60c1ca01c2a2db639f89d7e4bf7f654b9
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-06-07 01:07:17 -0500

    Add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  //for element button we have (event) with small case
+   templateUrl: './app.component.html',
+  /*
+    //for element button we have (event) with small case
 
-  //u can send the entire event as parameter ==> access in template with $Event
-  template : '<p> <input type="text" #myInput><br/><button (click)="clickme($event,myInput)">Click me</button>'+
-  '<p>test attr + event Binding </p>'+
-  //access variable in template directly , in component with this
-  '<p> <input type="text" [value]="attrBind" (input)="attrBind=$event.target.value"></p> {{attrBind}}'+
-  //[value] = propertyBinding => for bind attrBind to input
-  //(input) is event to call related method and change the value in {{}}
-  '<p>test TwoWay Binding </p>'+
-  '<p> <input [(ngModel)]="twoWayVar" (keyup.enter)="twoWayBinding()"> </p>',
+    //u can send the entire event as parameter ==> access in template with $Event
+    template : '<p> <input type="text" #myInput><br/><button (click)="clickme($event,myInput)">Click me</button>'+
+    '<p>test attr + event Binding </p>'+
+    //access variable in template directly , in component with this
+    '<p> <input type="text" [value]="attrBind" (input)="attrBind=$event.target.value"></p> {{attrBind}}'+
+    //[value] = propertyBinding => for bind attrBind to input
+    //(input) is event to call related method and change the value in {{}}
+    '<p>test TwoWay Binding </p>'+
+    '<p> <input [(ngModel)]="twoWayVar" (keyup.enter)="twoWayBinding()"> </p>',
+  */
 
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/item-detail/item-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-detail',
+  template: `
+    <p>
+      item-detail works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ItemDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
